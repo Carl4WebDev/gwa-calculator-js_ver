a4db601a0e0e52055346e88e5fccf8c1d949d384
carl Ivan Sampan <carlforwebdev@gmail.com>
2024-07-27 11:20:35 +0600

    better ui

--- index.js
+++ index.js
@@ -24,21 +24,38 @@ function resultBox(gradesInfo){
     const span = document.createElement('span')
     newBox.className = 'box row';
 
-    // const classDesignSubject = "fw-bold fs-6 d-flex flex-column text-break justify-content-evenly align-content-center"
-    // const classDesignGrades = "fw-bold fs-6 d-flex text-break justify-content-evenly align-content-center"
+    const classDesignSubject = "fw-bold fs-6 d-flex flex-column text-break justify-content-evenly align-content-center"
+    const classDesignGrades = "fw-bold fs-6 d-flex text-break justify-content-evenly align-content-center"
     // const classP = "fs-3 d-flex text-break justify-content-center align-content-center text-primary"
+    // const classDesignSubject = ""
+    // const classDesignGrades = ""
+    const classP = "text-break text-primary"
+    const classDesignFinalGrades = "fw-bolder fs-4 d-flex text-break justify-content-evenly align-content-center"
+    newBox.innerHTML = `
+        <div class="col-2"> 
+            <span class="${classDesignSubject}"><span>Subject:</span>      <p class="${classP}">${enterSubject.value} </p>   </span> 
+        </div>
+        <div class="col-2"> 
+            <span class="${classDesignGrades}"><span>Prelim:</span>        <p class="${classP}">${enterPrelim.value} </p>  </span> 
+        </div>
+        <div class="col-2"> 
+            <span class="${classDesignGrades}"><span>Midterm:</span>       <p class="${classP}">${enterMidterm.value} </p> </span> 
+        </div>
+        <div class="col-2"> 
+            <span class="${classDesignGrades}"><span>Prefinal:</span>      <p class="${classP}">${enterPrefinal.value} </p> </span> 
+        </div>
+        <div class="col-2"> 
+            <span class="${classDesignGrades}"><span>Finals:</span>        <p class="${classP}">${enterFinals.value} </p> </span> 
+        </div>
+        <div class="col-2"> 
+            <span class="${classDesignGrades}"><span>Course Unit:</span>  <p class="${classP}">${enterUnit.value} </p> </span> 
+        </div>
+        <div class="col-12 bg-warning justify-content-center align-content-center rounded"> 
+            <span class="${classDesignFinalGrades}"><span>Final Grade:</span>  <p>${90} </p> </span> 
+        </div>
 
-    // newBox.innerHTML = `
-    //     <span class="${classDesignSubject}"><span>Subject:</span>      <p class="${classP}">${enterSubject.value} </p>   </span> <br> <hr>
-    //     <span class="${classDesignGrades}"><span>Prelim:</span>        <p class="${classP}">${enterPrelim.value} </p>  </span> <br> <hr>
-    //     <span class="${classDesignGrades}"><span>Midterm:</span>       <p class="${classP}">${enterMidterm.value} </p> </span> <br> <hr>
-    //     <span class="${classDesignGrades}"><span>Prefinal:</span>      <p class="${classP}">${enterPrefinal.value} </p> </span> <br> <hr>
-    //     <span class="${classDesignGrades}"><span>Finals:</span>        <p class="${classP}">${enterFinals.value} </p> </span> <br> <hr>
-    //     <span class="${classDesignSubject}"><span>Course Unit:</span>  <p class="${classP}">${enterUnit.value} </p> </span> <br> <hr>
-
-    // `
-
-    newBox.innerHTML = `<span> ${enterSubject} - ${enterPrelim}</span>`
+    `
+    // newBox.innerHTML = `<span> ${enterSubject} - ${enterPrelim}</span> ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd`
     newBox.appendChild(span)
     resultBox.appendChild(newBox);
 }
